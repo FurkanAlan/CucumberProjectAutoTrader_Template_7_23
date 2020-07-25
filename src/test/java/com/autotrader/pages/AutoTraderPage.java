41395088a0b971257773f06ae3b2073e29d2ddcf
FurkanAlan <ulkirr@gmail.com>
2020-07-25 10:15:48 -0500

    CucumberProject Example

--- src/test/java/com/autotrader/pages/AutoTraderPage.java
+++ src/test/java/com/autotrader/pages/AutoTraderPage.java
@@ -6,14 +6,16 @@ import com.autotrader.utilities.MyDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.concurrent.TimeUnit;
+
 public class AutoTraderPage extends CommonMethods {
 
     @FindBy(xpath = "//html//body//header//div//div//nav//ul//li//a[contains(text(),'Used Cars')]")
     public WebElement usedCars;
 
 
-
-    public void clickOnUsedCars(){
+    public void clickOnUsedCars() {
+        MyDriver.get().manage().deleteAllCookies();
         usedCars.click();
         try {
             Thread.sleep(5000);
--- src/test/java/com/autotrader/stepDefinitions/AutoTraderSteps.java
+++ src/test/java/com/autotrader/stepDefinitions/AutoTraderSteps.java
@@ -8,15 +8,14 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 
 
-public class AutoTraderSteps extends CommonMethods {
+public class AutoTraderSteps {
+    AutoTraderPage autoTraderPage = new AutoTraderPage();
     @Given("User is in landing page")
     public void user_is_in_landing_page() {
-
+        autoTraderPage.clickOnUsedCars();
     }
 
     @Then("^I clicked on used cars on the home page$")
     public void i_Clicked_On_Used_Cars_On_The_HomePage() {
-//        MyDriver.get().manage().deleteAllCookies();
-        autoTraderPage.clickOnUsedCars();
     }
 }
